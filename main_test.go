74850ad08031dd4cab69b913e66e32ee6e5088d0
Tom Flanagan <tom@zkpq.ca>
2014-06-21 00:03:57 -0700

    deleted all of aaron's work

--- main_test.go
+++ main_test.go
@@ -1,76 +1,9 @@
 package domino
 
 import (
-	"github.com/knio/domino/tags"
+	// "github.com/knio/domino/tags"
 	. "testing"
 )
-
-func TestDocument(t *T) {
-	t.Parallel()
-	doc := Document("", nil, nil)
-
-	if doc.Title != nil {
-		t.Error("Expected no title node.")
-	}
-
-	if doc.Body.Name != tags.Body {
-		t.Error("Expected a body node.")
-	}
-
-	if doc.Head.Name != tags.Head {
-		t.Error("Expected a head node.")
-	}
-}
-
-func TestDocumentTitle(t *T) {
-	t.Parallel()
-	title := "testTitle"
-	doc := Document(title, nil, nil)
-
-	if doc.Title.Name != tags.Title {
-		t.Error("Expected a title node.")
-	}
-
-	if len(doc.Title.Children) == 0 {
-		t.Fatal("Title has no child nodes.")
-	}
-
-	if check := doc.Title.Children[0].Name; title != check {
-		t.Errorf("Expected a title with text: %v, got: %v", title, check)
-	}
-}
-
-func TestDocumentChildren(t *T) {
-	t.Parallel()
-	doc := Document("", Div(), Span())
-
-	if ln := len(doc.Head.Children); ln != 1 {
-		t.Fatal("Head must have exactly 1 child, has: ", ln)
-	}
-
-	if check := doc.Head.Children[0].Name; tags.Div != check {
-		t.Errorf("Expected a title with text: %v, got: %v", tags.Div, check)
-	}
-
-	if ln := len(doc.Body.Children); ln != 1 {
-		t.Fatal("Body must have exactly 1 child, has: ", ln)
-	}
-
-	if check := doc.Body.Children[0].Name; tags.Span != check {
-		t.Errorf("Expected a title with text: %v, got: %v", tags.Span, check)
-	}
-}
-
-func TestNode(t *T) {
-	t.Parallel()
-	if node := Node(tags.HTML); node == nil {
-		t.Error("Node should not be nil.")
-	} else if node.Name != tags.HTML {
-		t.Error("Name should be:", tags.HTML)
-	} else if node.Children == nil {
-		t.Error("It should pre-allocate the array of children.")
-	}
-}
 
 func TestDiv(t *T) {
 	t.Parallel()
@@ -84,7 +17,7 @@ func TestDiv(t *T) {
 }
 
 func TestAttrs(t *T) {
-	div := Div(Attr{"html": nil, "no": "\"quotes\"", "attr": "value"}).String()
+	div := Div(Attr{"html": nil, "no": "quotes", "attr": "value"}).String()
 	if div != `<div html no="quotes" attr="value"></div>` {
 		t.Error("Expected a div with several attributes, got: ", div)
 	}
@@ -97,55 +30,13 @@ func TestChildren(t *T) {
 }
 
 func TestSiblings(t *T) {
-	if div := Div().Span("hey").String(); div != "<div></div><span>hey</span>" {
-		t.Error("Expected a div next to a span with hey, got: ", div)
-	}
-}
-
-func TestAllTags(t *T) {
-	n := Node(tags.Body)
-
-	tag_tests := []struct {
-		name           string
-		handlerWithout func(...interface{}) *node
-		handlerWith    func(...interface{}) *node
-	}{
-		{tags.HTML, HTML, n.HTML},
-		{tags.Head, Head, n.Head},
-		{tags.Title, Title, n.Title},
-		{tags.Body, Body, n.Body},
-		{tags.Div, Div, n.Div},
-		{tags.Span, Span, n.Span},
-	}
-
-	for _, test := range tag_tests {
-		n.Left = nil
-		n.Right = nil
-
-		node := test.handlerWithout()
-		if name := node.Name; name != test.name {
-			t.Errorf("Tag FUNCTION incorrect, expected: %v, got: %v",
-				test.name, name)
-		}
-
-		node = test.handlerWith()
-		if name := node.Name; name != test.name {
-			t.Errorf("Tag METHOD incorrect, expected: %v, got: %v",
-				test.name, name)
-		}
-
-		if n.Right != node {
-			t.Error("Expected the left to have a link to the right:", n.Right)
-		}
-
-		if node.Left != n {
-			t.Error("Expected the right to have a link to the left:", n.Left)
-		}
+	if div := Div().Span("hey").String(); div != "<div><span>hey</span></div>" {
+		t.Error("Expected a span in a div, got: ", div)
 	}
 }
 
 func TestEscapes(t *T) {
 	t.Parallel()
-	_ = escapeHTMLArg
+	_ = HTMLEscape
 	t.SkipNow()
 }
--- tag_helpers.go
+++ tag_helpers.go
@@ -4,50 +4,56 @@ import (
 	"github.com/knio/domino/tags"
 )
 
-func HTML(args ...interface{}) *node {
-	return NodeArgs(tags.HTML, args...)
+func HTML(args ...interface{}) DomNode {
+	return MakeDomNode(tags.HTML, args...)
 }
 
-func (n *node) HTML(args ...interface{}) *node {
-	return n.setSibling(NodeArgs(tags.HTML, args...))
+func (n *domNode) HTML(args ...interface{}) DomNode {
+	 n.appendChild(MakeDomNode(tags.HTML, args...))
+	 return n
 }
 
-func Head(args ...interface{}) *node {
-	return NodeArgs(tags.Head, args...)
+func Head(args ...interface{}) DomNode {
+	return MakeDomNode(tags.Head, args...)
 }
 
-func (n *node) Head(args ...interface{}) *node {
-	return n.setSibling(NodeArgs(tags.Head, args...))
+func (n *domNode) Head(args ...interface{}) DomNode {
+	 n.appendChild(MakeDomNode(tags.Head, args...))
+	 return n
 }
 
-func Title(args ...interface{}) *node {
-	return NodeArgs(tags.Title, args...)
+func Title(args ...interface{}) DomNode {
+	return MakeDomNode(tags.Title, args...)
 }
 
-func (n *node) Title(args ...interface{}) *node {
-	return n.setSibling(NodeArgs(tags.Title, args...))
+func (n *domNode) Title(args ...interface{}) DomNode {
+	 n.appendChild(MakeDomNode(tags.Title, args...))
+	 return n
 }
 
-func Body(args ...interface{}) *node {
-	return NodeArgs(tags.Body, args...)
+func Body(args ...interface{}) DomNode {
+	return MakeDomNode(tags.Body, args...)
 }
 
-func (n *node) Body(args ...interface{}) *node {
-	return n.setSibling(NodeArgs(tags.Body, args...))
+func (n *domNode) Body(args ...interface{}) DomNode {
+	 n.appendChild(MakeDomNode(tags.Body, args...))
+	 return n
 }
 
-func Div(args ...interface{}) *node {
-	return NodeArgs(tags.Div, args...)
+func Div(args ...interface{}) DomNode {
+	return MakeDomNode(tags.Div, args...)
 }
 
-func (n *node) Div(args ...interface{}) *node {
-	return n.setSibling(NodeArgs(tags.Div, args...))
+func (n *domNode) Div(args ...interface{}) DomNode {
+	 n.appendChild(MakeDomNode(tags.Div, args...))
+	 return n
 }
 
-func Span(args ...interface{}) *node {
-	return NodeArgs(tags.Span, args...)
+func Span(args ...interface{}) DomNode {
+	return MakeDomNode(tags.Span, args...)
 }
 
-func (n *node) Span(args ...interface{}) *node {
-	return n.setSibling(NodeArgs(tags.Span, args...))
+func (n *domNode) Span(args ...interface{}) DomNode {
+	 n.appendChild(MakeDomNode(tags.Span, args...))
+	 return n
 }
